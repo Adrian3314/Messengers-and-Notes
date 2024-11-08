568650173eb2fb6ea35244bdec53a6d20c1ea916
Edison <z.921026@hotmail.com>
2024-11-08 11:54:05 +0800

    添加註解

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MessengerAndNotes.db'
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MessengerAndNotes.db' # 資料庫名稱
+db = SQLAlchemy(app) # 建立資料庫物件
 
-@app.route('/')
+@app.route('/') # 路由
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html') # 回傳 home.html
 
-if __name__ == '__main__':
+if __name__ == '__main__': # 程式進入點
     app.run(debug=True)
