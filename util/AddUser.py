63f90448bdabfe4e291d25565eec00e92da9fde5
Edison <z.921026@hotmail.com>
2024-11-21 15:48:44 +0800

    Fix AddUser.

--- util/AddUser.py
+++ util/AddUser.py
@@ -7,5 +7,5 @@ predefine = [
 if __name__ == "__main__":
     db = sqlite3.connect('MessengerAndNotes.db')
     cursor = db.cursor()
-    cursor.executemany("INSERT INTO courses VALUES(?, ?, ?)", predefine)
+    cursor.executemany("INSERT INTO user VALUES(?, ?, ?)", predefine)
     db.commit()
