28fa969d7e28a1ea7eabd6f2c05d95373722b260
Edison <z.921026@hotmail.com>
2024-12-15 14:35:11 +0800

    addUser change.

--- util/AddUser.py
+++ util/AddUser.py
@@ -3,7 +3,10 @@ from datetime import datetime
 
 predefineUser = [
     (1, 'user1', '123'),
-    (2, 'user2', '123')
+    (2, 'user2', '123'),
+    (3, 'user3', '123'),
+    (4, 'user4', '123'),
+    (5, 'user5', '123')
 ]
 
 predefineRoom = [
@@ -11,11 +14,15 @@ predefineRoom = [
 ]
 
 predefineMessage = [
-    (1, datetime)
+    # (1, datetime(???), "Hi"), 
+    (1, datetime(), "Hello"),
+    (1, datetime(), "Hey")
 ]
 
 if __name__ == "__main__":
     db = sqlite3.connect('MessengerAndNotes.db')
     cursor = db.cursor()
-    cursor.executemany("INSERT INTO user VALUES(?, ?, ?)", predefine)
+    cursor.executemany("INSERT INTO user VALUES(?, ?, ?)", predefineUser)
+    cursor.executemany("INSERT INTO room VALUES(?, ?, ?)", predefineRoom)
+    cursor.executemany("INSERT INTO message VALUES(?, ?, ?)", predefineMessage)
     db.commit()
